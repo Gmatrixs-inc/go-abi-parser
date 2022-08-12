da8aa4a9198e512ba6295f700f273ddefa0bb32f
Gmatrixs <94102544+Gmatrixs@users.noreply.github.com>
2022-08-12 12:07:17 +0800

    Add files via upload

--- cmd/eth.go
+++ cmd/eth.go
@@ -1,9 +1,59 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"gabiparser/ethclient"
+	"gabiparser/hethd"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"strings"
 )
+
+func logFilter(startI, endI int64) {
+	rpcUri := viper.GetString("Eth.Rpcurl")
+	ethclient.InitClient(rpcUri)
+
+	configTokenRowAddresses := []string{viper.GetString("Eth.ContractAddress")}
+	contractAbi, err := abi.JSON(strings.NewReader(ethclient.ERC721ABI))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var tokeinds []string
+	for i := startI; i < endI; i++ {
+		if len(configTokenRowAddresses) > 0 {
+			logs, err := ethclient.RpcFilterLogs(
+				context.Background(),
+				i,
+				i,
+				configTokenRowAddresses,
+				contractAbi.Events["Transfer"],
+			)
+			if err != nil {
+				return
+			}
+
+			for _, log := range logs {
+				txid := log.TxHash.Hex()
+				toaddr := hethd.CommonHashToAddrssStringLower(log.Topics[2])
+				fromaddr := hethd.CommonHashToAddrssStringLower(log.Topics[1])
+				tokenId, ok := math.ParseBig256(log.Topics[3].Hex())
+				if !ok {
+					fmt.Printf("invalid hex or decimal integer %d", tokenId)
+				}
+
+				if fromaddr == "0x0000000000000000000000000000000000000000" {
+					tokeinds = append(tokeinds, tokenId.String())
+				}
+				fmt.Println(txid, fromaddr, toaddr, tokenId)
+			}
+			fmt.Println(len(tokeinds))
+		}
+	}
+}
 
 var ethCmd = &cobra.Command{
 	Use:   "eth",
@@ -11,5 +61,6 @@ var ethCmd = &cobra.Command{
 	Long:  `Eth log parser`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("eth log parser")
+
 	},
 }
--- main.go
+++ main.go
@@ -1,56 +1,14 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"gabiparser/ethclient"
-	"gabiparser/hethd"
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/common/math"
-	"strings"
+	"gabiparser/cmd"
 )
 
 func main() {
-	rpcUri := "https://mainnet.infura.io/v3/ee07e33cb6414781a72deaf3b303ca3b"
-	ethclient.InitClient(rpcUri)
-
-	configTokenRowAddresses := []string{"0x06012c8cf97BEaD5deAe237070F9587f8E7A266d"}
-	contractAbi, err := abi.JSON(strings.NewReader(ethclient.ERC721ABI))
+	fmt.Println("Start Processing...")
+	err := cmd.Execute()
 	if err != nil {
-		fmt.Println(err)
-	}
-
-	startI := int64(1)
-	endI := int64(1000)
-	var tokeinds []string
-	for i := startI; i < endI; i++ {
-		if len(configTokenRowAddresses) > 0 {
-			logs, err := ethclient.RpcFilterLogs(
-				context.Background(),
-				i,
-				i,
-				configTokenRowAddresses,
-				contractAbi.Events["Transfer"],
-			)
-			if err != nil {
-				return
-			}
-
-			for _, log := range logs {
-				txid := log.TxHash.Hex()
-				toaddr := hethd.CommonHashToAddrssStringLower(log.Topics[2])
-				fromaddr := hethd.CommonHashToAddrssStringLower(log.Topics[1])
-				tokenId, ok := math.ParseBig256(log.Topics[3].Hex())
-				if !ok {
-					fmt.Printf("invalid hex or decimal integer %d", tokenId)
-				}
-
-				if fromaddr == "0x0000000000000000000000000000000000000000" {
-					tokeinds = append(tokeinds, tokenId.String())
-				}
-				fmt.Println(txid, fromaddr, toaddr, tokenId)
-			}
-			fmt.Println(len(tokeinds))
-		}
+		return
 	}
 }
