f30d96fa4810d89f42a103c0739ae5670d8ee4d2
Gmatrixs <94102544+Gmatrixs@users.noreply.github.com>
2022-09-09 10:22:37 +0800

    Add files via upload

--- cmd/root.go
+++ cmd/root.go
@@ -30,13 +30,13 @@ func init() {
 	rootCmd.PersistentFlags().StringP("author", "a", "YOUR NAME", "author name for copyright attribution")
 	rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of license for the project")
 	rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
-	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
-	viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
+
 	viper.SetDefault("author", "NAME HERE <EMAIL ADDRESS>")
 	viper.SetDefault("license", "apache")
 
 	rootCmd.AddCommand(versionCmd)
 	rootCmd.AddCommand(ethCmd)
+	rootCmd.AddCommand(bscCmd)
 }
 
 func initConfig() {
